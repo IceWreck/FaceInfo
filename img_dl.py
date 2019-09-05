5d169a0f1aa299ed205f7095442ffeed28a17355
Anchit <ab@abifog.com>
2019-09-05 23:43:34 +0530

    now image borders work

---
+++ img_dl.py
@@ -0,0 +1,12 @@
+import wget
+import time
+
+def download_image(image_link):
+    img_name = str(time.time()).split('.')[0]
+    out_filepath = "img_dl/" + img_name + ".jpg"
+    down_file = wget.download(image_link, out=out_filepath)
+    return img_name
+
+# Usage
+# print(download_image('https://upload.wikimedia.org/wikipedia/commons/3/37/Dagestani_man_and_woman.jpg'))
+# Returns just the filename without path or extension
